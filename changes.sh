c820ad648ea87e0ada26e6ec8624ae276df749dd
Chirag Arti Sharma <chip2741@gmail.com>
2023-11-07 10:59:53 +0530

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
-auth=""
+auth="d6165047b19c9421729ea50b34a389f676338173d50960aa829cd6db7899a07c"
 #zabbix_url="{$ZABBIX_URL}"
-zbx_host=""
+zbx_host="operations.ops.ped.local"
 zabbix_url="https://${zbx_host}/api_jsonrpc.php"
 echo $zabbix_url
 Dash_name="WPTT"
@@ -9,7 +9,6 @@ existing_dash=$(curl -k -X POST -H "Content-Type: application/json" -d '{"jsonrp
 # existing_dash=$(curl -k -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "dashboard.get", "params": {"output": "extend", "selectPages": "extend", "selectUsers": "extend", "selectUserGroups": "extend", "dashboardids": ["226"]},"id": 2, "auth": "'$auth'"}' "$zabbix_url")
 data_file="/tmp/zabbix_dash.json"
 
-# color=$(printf "%02x%02x%02x\n" $((RANDOM%128)) $((RANDOM%128)) $((RANDOM%128)))
 
 ##################################### Json Part1 ######################################################
 case ${existing_dash} in
@@ -35,24 +34,27 @@ echo -n "$json_part1" >> $data_file
 esac
 
 ##################################### Json Part2 ######################################################
-svggraph_type_list="Threading: Thread Count;Threading: Daemon thread count;Memory: Heap memory maximum size"
+svggraph_type_list="Threading: Thread Count;Threading: Daemon thread count;Memory: Heap memory maximum size;Memory: Heap memory used;Memory: Non-Heap memory used;Memory: Heap memory committed;Memory: Non-Heap memory committed;Threading: Total started thread count;Threading: Peak thread count;OperatingSystem: File descriptors opened;OperatingSystem: Process CPU Load"
 pattern="eu-we1-*.ppe.wpt.local"
+##################################### Json Part2 ######################################################
+top_host_type_list=
 place=0
-x_position=0
-y_position=0
+x=0
+y=0
 
 IFS=";"
 for type in ${svggraph_type_list}
 do
     # Calculate x and y positions
     if (( place % 2 == 0 )); then
-        x_position=0
-        y_position=$(( (place / 2) * 6 ))
+        x=0
+        y=$(( (place / 2) * 6 ))
     else
-        x_position=12 # Assuming the width of each graph is 12
+        x=12 # Assuming the width of each graph is 12
     fi
-
-    json_part2='{"type":"svggraph","name":"'$type'","x":"'$x_position'","y":"'$y_position'","width":"12","height":"6","view_mode":"0","fields":[{"type":"0","name":"ds.transparency.0","value":"1"},{"type":"0","name":"ds.fill.0","value":"2"},{"type":"0","name":"righty","value":"0"},{"type":"1","name":"ds.hosts.0.0","value":"'$pattern'"},{"type":"1","name":"ds.items.0.0","value":"'$type'"},{"type":"0","name":"ds.type.0","value":"2"},{"type":"0","name":"ds.width.0","value":"4"},{"type":"1","name":"ds.color.0","value":"5E35B1"}]},'
+    color=$(printf "%02x%02x%02x\n" $((RANDOM%128)) $((RANDOM%128)) $((RANDOM%128)))
+    # color=$(echo "$(openssl rand -hex 3)")
+    json_part2='{"type":"svggraph","name":"'$type'","x":"'$x'","y":"'$y'","width":"12","height":"6","view_mode":"0","fields":[{"type":"0","name":"ds.transparency.0","value":"1"},{"type":"0","name":"ds.fill.0","value":"2"},{"type":"0","name":"righty","value":"0"},{"type":"1","name":"ds.hosts.0.0","value":"'$pattern'"},{"type":"1","name":"ds.items.0.0","value":"'$type'"},{"type":"0","name":"ds.type.0","value":"2"},{"type":"0","name":"ds.width.0","value":"4"},{"type":"1","name":"ds.color.0","value":"'$color'"}]},'
     echo -n "$json_part2" >> $data_file
 
     # Increment place for the next widget's position
@@ -65,5 +67,4 @@ echo -n "$json_final" >> $data_file
 curl -k -X POST -H "Content-Type: application/json" --data @$data_file "$zabbix_url"
 
 
-#{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid params.","data":"Overlapping widgets at X:0, Y:9 on page #1 of dashboard \"WPTT\".","debug":[{"file":"/usr/share/zabbix/include/classes/api/services/CDashboardGeneral.php","line":337,"function":"exception","class":"CApiService","type":"::"},{"file":"/usr/share/zabbix/include/classes/api/services/CDashboard.php","line":270,"function":"checkWidgets","class":"CDashboardGeneral","type":"->"},{"file":"/usr/share/zabbix/include/classes/api/services/CDashboard.php","line":160,"function":"validateCreate","class":"CDashboard","type":"->"},{"file":"/usr/share/zabbix/include/classes/api/clients/CLocalApiClient.php","line":121,"function":"create","class":"CDashboard","type":"->"},{"file":"/usr/share/zabbix/include/classes/core/CJsonRpc.php","line":75,"function":"callMethod","class":"CLocalApiClient","type":"->"},{"file":"/usr/share/zabbix/api_jsonrpc.php","line":63,"function":"execute","class":"CJsonRpc","type":"->"}]},"id":1}
-#{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid params.","data":"Overlapping widgets at X:0, Y:13 on page #1 of dashboard \"WPTT\".","debug":[{"file":"/usr/share/zabbix/include/classes/api/services/CDashboardGeneral.php","line":337,"function":"exception","class":"CApiService","type":"::"},{"file":"/usr/share/zabbix/include/classes/api/services/CDashboard.php","line":270,"function":"checkWidgets","class":"CDashboardGeneral","type":"->"},{"file":"/usr/share/zabbix/include/classes/api/services/CDashboard.php","line":160,"function":"validateCreate","class":"CDashboard","type":"->"},{"file":"/usr/share/zabbix/include/classes/api/clients/CLocalApiClient.php","line":121,"function":"create","class":"CDashboard","type":"->"},{"file":"/usr/share/zabbix/include/classes/core/CJsonRpc.php","line":75,"function":"callMethod","class":"CLocalApiClient","type":"->"},{"file":"/usr/share/zabbix/api_jsonrpc.php","line":63,"function":"execute","class":"CJsonRpc","type":"->"}]},"id":1}
+
